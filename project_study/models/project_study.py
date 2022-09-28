32586732fc98074e5d6c269657166e32e84db149
tronghung1995 <pthung240495@gmail.com>
2022-09-28 17:12:45 +0700

    update

--- project_study/models/project_study.py
+++ project_study/models/project_study.py
@@ -6,7 +6,7 @@ class Project_study(models.Model):
 
     name = fields.Char(string="Name", required="True")
     dateline = fields.Date(string="DateLine", required="True")
-    assign_to = fields.Many2one("res.partner", string="Assigned To")
+    assign_to = fields.Many2one("res.users", string="Assigned To")
     note = fields.Text(string='Note')
     description = fields.Html(string="Description")
     state = fields.Selection([('TODO', 'To Do'), ('progress', 'In-progress'), ('review', 'Review'), ('done', 'Done')])
